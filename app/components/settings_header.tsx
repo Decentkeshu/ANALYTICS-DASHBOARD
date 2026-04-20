18bdc7f862ea81a1ec36827aa2d2c7895b3b5c8a
keshav kumar <msd76017@gmail.com>
2026-04-20 23:48:21 +0530

    fianl update

---
+++ app/components/settings_header.tsx
@@ -0,0 +1,55 @@
+"use client"
+import { useState } from "react"
+import { Calendar } from "lucide-react"
+import { User, Settings, Bell } from "lucide-react";
+
+const filters = [
+  { label: "Last 7 days",  value: "7d"  },
+  // { label: "Last 30 days", value: "30d" },
+  // { label: "Last 90 days", value: "90d" },
+  // { label: "This year",    value: "1y"  },
+]
+
+type Props = {
+  onChange?: (value: string) => void
+}
+
+export default function SettingsHeader({ onChange }: Props) {
+  const [active, setActive] = useState("30d")
+
+  const handleSelect = (value: string) => {
+    setActive(value)
+    onChange?.(value)
+  }
+
+  return (
+    <div className="flex items-center justify-between flex-wrap  bg-gray-100 p-2">
+
+  
+      <div className="flex">
+        <h1 className="text-base font-medium text-gray-800">Settings</h1>
+        
+        {/* <p className="text-xs text-gray-400 mt-0.5">Track</p> */}
+      </div>
+
+      <div className="flex items-center gap-1">
+        {/* <Calendar size={14} className="text-gray-400" /> */}
+        <div className="flex items-center bg-gray-100 rounded-lg p-1 gap-1">
+
+          <div className="flex ">  
+           <Settings size={24}  className="mr-5"/>
+
+      {/* Bell with notification badge */}
+      <div style={{ position: "relative" }}>
+        <Bell size={24} className="mr-5" />
+        <span className="badge" />
+      </div>
+
+      <User size={24} />
+      </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,5 +1,9 @@
+import SettingsHeader from "../components/settings_header";
+
 export default function Settings(){
-    return<div className="analytics">
-      <h1>Settings page.</h1>
+    return<div className="pt-4 px-6 pb-6 flex flex-col gap-4 flex-1">
+      <div className="col-span-1">  
+      <SettingsHeader />
+      </div>
     </div>
 }
